67fc3a8a1da2529bfc6792762bdc914868de7031
vijeth k gowda <vijethkgowda@gmail.com>
2020-11-01 19:12:14 +0530

    cache removed

--- src/components/Monthly/Monthly.js
+++ src/components/Monthly/Monthly.js
@@ -59,23 +59,25 @@ const Monthly = ({ showDate, selectedDate, events }) => {
                           item.today ?
                             <div className="today">
                               {item.day?.date()}
-                            </div> : (item.day?.date())
+                            </div> : <div className="other-day">{(item.day?.date())}</div>
                         }
                       </div>
-                      {
-                        events.map((eve) => {
-                          return (
-                            <div className="eventBox" key={eve.from}>{
-                              eve.from?.date() == item.day?.date() &&
-                                eve.from?.month() == item.day?.month() &&
-                                eve.from?.year() == item.day?.year() ?
-                                <button className="eventTitle">
-                                  {eve.title}
-                                </button> : null
-                            }</div>
-                          )
-                        })
-                      }
+                      <div className="scrollable">
+                        {
+                          events.map((eve) => {
+                            return (
+                              <div className="eventBox" key={eve.from}>{
+                                eve.from?.date() == item.day?.date() &&
+                                  eve.from?.month() == item.day?.month() &&
+                                  eve.from?.year() == item.day?.year() ?
+                                  <button className="eventTitle">
+                                    {eve.title}
+                                  </button> : null
+                              }</div>
+                            )
+                          })
+                        }
+                      </div>
                     </div>
                     :
                     <div key={index} className={classNames('dateBox', { 'disabled': !Boolean(item.day) })}></div>
